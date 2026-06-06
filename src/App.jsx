4d0e05a0e77f74717dd6e3970bba937dcd348675
venkatesh-window <gsvenkatesh963@gmail.com>
2026-06-06 14:51:26 +0530

    Pages arrangement

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import Home from './pages/home.jsx';
+import About from './pages/about.jsx';
 
 function App() {
 
-
   return (
-    
-  )
+    <>
+    <Home />
+    <About />
+    </>
+  );
 }
 
-export default App
+export default App;
